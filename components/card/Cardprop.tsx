4d283e16bca1c6ec93a0e6371688870df17c4eff
vinhntph16238 <vinhntph16238@fpt.edu.vn>
2023-05-14 18:26:42 +0700

    vinhvinh

---
+++ components/card/Cardprop.tsx
@@ -0,0 +1,4 @@
+export type CardProps = {
+    title : String;//tiêu đề của card
+    imgUrl: any; //đường dẫn ảnh của card
+}
---
+++ styles/mainstyles.js
@@ -0,0 +1,30 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 100,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    fontBold: {
+        fontWeight: 'bold',
+
+    },
+    fontSemibold: {
+        fontWeight: '400',
+
+    },
+    red: {
+        fontSize: 30,
+        color: 'red',
+    },
+    blue: {
+        fontSize: 20,
+        color: 'blue',
+    },
+    image: {
+        width: 200,
+        height: 200,
+    },
+});
+export default styles;
